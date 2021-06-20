a47422cf27b81e3908e378fa529f50834f48fe14
Artur Vrublevskiy <v8a8rt@gmail.com>
2021-06-20 12:04:13 +0300

    gulp updated

---
+++ gulp.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    SRC_PATH : 'app/src',
+    DIST_PATH : 'app/dist',
+    STYLE_LIBS : [
+      'node_modules/normalize.css/normalize.css'
+    ],
+    JS_LIBS : [
+      'node_modules/jquery/dist/jquery.js'
+    ]
+   }
--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,122 @@
-var gulp          = require('gulp'),
-	gutil         = require('gulp-util' ),
-	sass          = require('gulp-sass'),
-	browsersync   = require('browser-sync'),
-	concat        = require('gulp-concat'),
-	uglify        = require('gulp-uglify'),
-	cleancss      = require('gulp-clean-css'),
-	rename        = require('gulp-rename'),
-	autoprefixer  = require('gulp-autoprefixer'),
-	notify        = require("gulp-notify"),
-	rsync         = require('gulp-rsync'),
-	wait	      = require('gulp-wait'),
-	sourcemaps    = require('gulp-sourcemaps');
-
-gulp.task('browser-sync', function() {
-	browsersync({
-		server: {
-			baseDir: 'app'
-		},
-		notify: false,
-		// open: false,
-		// tunnel: true,
-		// tunnel: "projectname", //Demonstration page: http://projectname.localtunnel.me
-	})
+const { src, dest, task, series, watch, parallel } = require("gulp");
+const rm = require('gulp-rm');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const sassGlob = require('gulp-sass-glob');
+const autoprefixer = require('gulp-autoprefixer');
+const px2rem = require('gulp-smile-px2rem');
+const gcmq = require('gulp-group-css-media-queries');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const svgo = require('gulp-svgo');
+const svgSprite = require('gulp-svg-sprite');
+const gulpif = require('gulp-if');
+ 
+const env = process.env.NODE_ENV;
+ 
+const {SRC_PATH, DIST_PATH, STYLE_LIBS, JS_LIBS} = require('./gulp.config');
+ 
+sass.compiler = require('node-sass');
+ 
+task('clean', () => {
+ return src(`${DIST_PATH}/**/*`, { read: false })
+   .pipe(rm())
+})
+ 
+task('copy:html', () => {
+ return src(`${SRC_PATH}/*.html`)
+   .pipe(dest(DIST_PATH))
+   .pipe(reload({ stream: true }));
+})
+ 
+task('styles', () => {
+ return src([...STYLE_LIBS, 'app/scss/main.scss'])
+   .pipe(gulpif(env === 'dev', sourcemaps.init()))
+   .pipe(concat('main.min.scss'))
+   .pipe(sassGlob())
+   .pipe(sass().on('error', sass.logError))
+//    .pipe(px2rem())
+   .pipe(gulpif(env === 'prod', autoprefixer({
+       browsers: ['last 2 versions'],
+       cascade: false
+     })))
+   .pipe(gulpif(env === 'prod', gcmq()))
+   .pipe(gulpif(env === 'prod', cleanCSS()))
+   .pipe(gulpif(env === 'dev', sourcemaps.write()))
+   .pipe(dest(DIST_PATH))
+   .pipe(reload({ stream: true }));
 });
-
-gulp.task('styles', function() {
-	return gulp.src('app/scss/main.scss')
-	.pipe(wait(1500))
-	.pipe(sourcemaps.init({loadMaps: true}))
-		.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
-		// .pipe(rename({ suffix: '.min', prefix : '' }))
-		.pipe(autoprefixer(['last 15 versions']))
-		// .pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest('app/css'))
-	.pipe(browsersync.reload( {stream: true} ))
+ 
+const libs = [
+ 'node_modules/jquery/dist/jquery.js',
+ 'src/scripts/*.js'
+];
+ 
+task('scripts', () => {
+ return src([...JS_LIBS, 'src/scripts/*.js'])
+   .pipe(gulpif(env === 'dev', sourcemaps.init()))
+   .pipe(concat('main.min.js', {newLine: ';'}))
+   .pipe(gulpif(env === 'prod', babel({
+       presets: ['@babel/env']
+     })))
+   .pipe(gulpif(env === 'prod', uglify()))
+   .pipe(gulpif(env === 'dev', sourcemaps.write()))
+   .pipe(dest(DIST_PATH))
+   .pipe(reload({ stream: true }));
 });
-
-gulp.task('js', function() {
-	return gulp.src([
-		'app/js/module.js', // Always at the end
-		])
-	.pipe(sourcemaps.init({loadMaps: true}))
-		.pipe(concat('app.min.js'))
-		//.pipe(uglify()) // Mifify js (opt.)
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest('app/js'))
-	.pipe(browsersync.reload({ stream: true }))
+ 
+task('icons', () => {
+ return src('src/images/icons/*.svg')
+   .pipe(svgo({
+     plugins: [
+       {
+         removeAttrs: {
+           attrs: '(fill|stroke|style|width|height|data.*)'
+         }
+       }
+     ]
+   }))
+   .pipe(svgSprite({
+     mode: {
+       symbol: {
+         sprite: '../sprite.svg'
+       }
+     }
+   }))
+   .pipe(dest(`${DIST_PATH}/images/icons`));
 });
-
-gulp.task('rsync', function() {
-	return gulp.src('app/**')
-	.pipe(rsync({
-		root: 'app/',
-		hostname: 'username@yousite.com',
-		destination: 'yousite/public_html/',
-		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
-		recursive: true,
-		archive: true,
-		silent: false,
-		compress: true
-	}))
+ 
+task('server', () => {
+ browserSync.init({
+     server: {
+         baseDir: "./dist"
+     },
+     open: false
+ });
 });
-
-gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
-	gulp.watch('app/scss/**/*.scss', ['styles']);
-	gulp.watch(['libs/**/*.js', 'app/js/*.js'], ['js']);
-	gulp.watch('app/*.html', browsersync.reload)
+ 
+task('watch', () => {
+ watch('./src/styles/**/*.scss', series('styles'));
+ watch('./src/*.html', series('copy:html'));
+ watch('./src/scripts/*.js', series('scripts'));
+ watch('./src/images/icons/*.svg', series('icons'));
 });
-
-gulp.task('default', ['watch']);
+ 
+ 
+task('default',
+ series(
+   'clean',
+   parallel('copy:html', 'styles', 'scripts', 'icons'),
+   parallel('watch', 'server')
+ )
+);
+ 
+task('build',
+ series(
+   'clean',
+   parallel('copy:html', 'styles', 'scripts', 'icons'))
+);
